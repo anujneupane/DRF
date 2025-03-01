6a881cc5ed9fd4540abc1fd0b5e1063e263c9abd
anujneupane <anujneupane987@gmail.com>
2025-03-02 00:25:19 +0545

    ''

--- APiView/api/views.py
+++ APiView/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import student
 from .serializer import StudentSerializer
 from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
 
 
