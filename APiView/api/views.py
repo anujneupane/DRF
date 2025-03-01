bbef90736cbb3b7e7e74cbd6142ff008bec8ed62
anujneupane <anujneupane987@gmail.com>
2025-03-02 01:24:05 +0545

    'classbasedapi'

--- APiView/api/views.py
+++ APiView/api/views.py
@@ -8,16 +8,10 @@ from rest_framework.views import APIView
 # Create your views here.
 
 
-# @api_view(['GET','POST'])
-# def testing(request):
-#     if request.method =='GET':    
-#       return Response('Get aayo hai api view bata')
-    
-#     if request.method =='POST':  
-#       print(request.data)  
-#       return Response('Post aayo hai api view bata')
 
-@api_view(['GET','POST','PUT','PATCH','DELETE'])
+
+#function based api view
+@api_view(['GET','POST','PUT','PATCH','DELETE'])   
 def testing(request , pk=None):
     if request.method =='GET':  
        id = pk #request.data.get('id')
@@ -60,11 +54,56 @@ def testing(request , pk=None):
       stu.delete()
       return Response({'msg':'data deleted of id '+str(id)})
 
+#class based API view
 
-       
+'''class StudentAPI(APIView):
+   def get(self,request,format=None,pk=None):
+      id = pk #request.data.get('id')
+      if id is not None:
+           stu = student.objects.get(id=id)
+           serial = StudentSerializer(stu)
+           return Response(serial.data)
+      stu = student.objects.all()
+      serial = StudentSerializer(stu,many = True)
+      return Response(serial.data)
+   
+   def post(self,request,format=None):
+       serial =StudentSerializer(data = request.data)
+       if serial.is_valid():
+           serial.save()
+           return Response({'msg': 'Data Created'},status = status.HTTP_201_CREATED)
+       return Response(serial.errors,status = status.HTTP_400_BAD_REQUEST)
   
-
+   def put(self,request,pk,format=None):
+      id = pk
+      stu = student.objects.get(pk=id)
+      serial =StudentSerializer(stu,data=request.data)
+      if serial.is_valid():
+           serial.save()
+           return Response({'msg': 'Complete Data Updated'})
+      return Response(serial.errors,status = status.HTTP_400_BAD_REQUEST)
+   
+   def patch(self,request,pk,format=None):
+      id = pk #request.data.get('id')
+      stu = student.objects.get(pk=id)
+      serial =StudentSerializer(stu,data=request.data,partial= True)
+      if serial.is_valid():
+           serial.save()
+           return Response({'msg': 'Partial Data Updated'})
+      return Response(serial.errors,status = status.HTTP_400_BAD_REQUEST)
        
+   def delete(self,request,pk,format=None):  
+      id = pk #request.data.get('id')
+      stu = student.objects.get(pk=id)
+      stu.delete()
+      return Response({'msg':'data deleted of id '+str(id)}) '''
+  
      
-
+'''@api_view(['GET','POST'])
+  def testing(request):
+    if request.method =='GET':    
+      return Response('Get aayo hai api view bata')
     
+    if request.method =='POST':  
+      print(request.data)  
+      return Response('Post aayo hai api view bata')'''
