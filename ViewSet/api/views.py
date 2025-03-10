a756c7f8ac8abeb07dc4128afce25543208ba5a9
anujneupane <anujneupane987@gmail.com>
2025-03-10 14:22:26 +0545

    router-url

--- ViewSet/api/views.py
+++ ViewSet/api/views.py
@@ -1,53 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from .serializer import UserSerializer
 from .models import User
 from rest_framework.response import Response
 from rest_framework import status
 
-# Create your views here.
-
-class userViewSets(viewsets.ViewSet):
-    def List(self,request):
-        user = User.objects.all()
-        serializer = UserSerializer(user,many = True)
+class UserViewSet(viewsets.ViewSet):
+    def list(self, request):  
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
     
-    def retrive(self,request,pk=None):
-        if pk is not None:
-            user = User.objects.get(id = pk)
-            serializer = UserSerializer(user)
-            return Response(serializer.data)
+    def retrieve(self, request, pk=None): 
+        user = get_object_or_404(User, pk=pk)  #  Handle 404 errors safely
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
     
-    def create(self,request):
-        serial = UserSerializer(data = request.data)
-        if serial.is_valid():
-            serial.save()
-            return Response({'msg':'Data Created'},status = status.HTTP_201_CREATED)
-        return Response(serial.errors,status = status.HTTP_400_BAD_REQUEST)
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Created'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def update(self,request,pk):
-        id = pk
-        user = user.objects.get(pk = id)
-        serial = UserSerializer(user,data = request.data)
-        if serial.is_valid():
-            serial.save()
-            return Response({'msg':'Complate Data Updated'})
-        return Response(serial.errors,status = status.HTTP_400_BAD_REQUEST)
+    def update(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)  # Fix model reference
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Complete Data Updated'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def partial_update(self,request,pk):
-        id = pk
-        user = user.objects.get(pk = id)
-        serial = UserSerializer(user,data = request.data,partial = True)
-        if serial.is_valid():
-            serial.save()
-            return Response({'msg':'Partial Data Updated'})
-        return Response(serial.errors,status = status.HTTP_400_BAD_REQUEST)
+    def partial_update(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)  # Fix model reference
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Partial Data Updated'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def destroy(self,request,pk):
-        id = pk
-        user = user.objects.get(pk = id)
+    def destroy(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)  # Fix model reference
         user.delete()
         return Response({'msg': 'Data Deleted'})
-
-
