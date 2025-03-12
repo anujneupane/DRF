4b3d160377324958635396509df559e4b6f81af5
anujneupane <anujneupane987@gmail.com>
2025-03-12 16:31:04 +0545

    auth-perm-inFunctionBasedView

--- APiView/api/views.py
+++ APiView/api/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,authentication_classes,permission_classes
 from rest_framework.response import Response
 from .models import student
 from .serializer import StudentSerializer
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
 # Create your views here.
 
 
 #function based api view
 @api_view(['GET','POST','PUT','PATCH','DELETE'])   
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def testing(request , pk=None):
     if request.method =='GET':  
        id = pk #request.data.get('id')
