44aa1181b190bd95b5aa4cfe583b8c4e838370d7
anujneupane <anujneupane987@gmail.com>
2025-03-16 23:49:07 +0545

    going

--- filtering/api/views.py
+++ filtering/api/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 from .serializer import RecordSerializer
+from rest_framework.generics import ListAPIView
 
 # Create your views here.
+class RecoredApi(ListAPIView):
+    serializer_class = RecordsSerializer
+    queryset = Record.objects.all()
