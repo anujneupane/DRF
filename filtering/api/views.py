13062a8b992e808314d79358fb0253fcf085f21a
anujneupane <anujneupane987@gmail.com>
2025-03-17 12:48:15 +0545

    djangofilterbackend

--- filtering/api/views.py
+++ filtering/api/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from .serializer import RecordSerializer
 from rest_framework.generics import ListAPIView
 from .models import Record
-
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 class RecoredApi(ListAPIView):
     serializer_class = RecordSerializer
     queryset = Record.objects.all()
-    def get_queryset(self):
+    filter_backends = [DjangoFilterBackend] 
+    filterset_fields = ['passby','city','roll']
+
+
+    ''' def get_queryset(self):
         user = self.request.user
-        return Record.objects.filter(passby = user)
+        return Record.objects.filter(passby = user) '''
