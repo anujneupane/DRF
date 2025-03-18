e9fb04f5debe82895e862ad7f6ecb8350e3ce32d
anujneupane <anujneupane987@gmail.com>
2025-03-18 23:41:42 +0545

    pagepagination

--- pagination/api/views.py
+++ pagination/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import User
+from .serializer import UserSerializer
 
-# Create your views here.
+
+class userList(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
