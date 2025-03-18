97462c294607e97b97cd9f8f74c41dcf1400ee91
anujneupane <anujneupane987@gmail.com>
2025-03-18 23:59:39 +0545

    param

--- pagination/api/views.py
+++ pagination/api/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import User
 from .serializer import UserSerializer
+from rest_framework.pagination import PageNumberPagination
+
+class MyPage(PageNumberPagination):
+    page_size = 7
+    max_page_size = 5
+    page_query_param = 'p'
+
 
 
 class userList(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    pagination_class = MyPage
