6072401d18f1f8aa7ab97a60eb48aa92cbde9f05
anujneupane <anujneupane987@gmail.com>
2025-03-18 14:21:43 +0545

    add-orderingFields

--- filtering/api/views.py
+++ filtering/api/views.py
@@ -4,15 +4,23 @@ from rest_framework.generics import ListAPIView
 from .models import Record
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 # Create your views here.
 class RecoredApi(ListAPIView):
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
+
     #filter_backends = [DjangoFilterBackend] 
     #filterset_fields = ['passby','city','roll']
-    filter_backends = [SearchFilter] 
+    
+    #filter_backends = [SearchFilter] 
     #search_fields = ['city','name']
-    search_fields = ['^city','^name']  # ^ for start with of city and name
+    #search_fields = ['^city','^name']  # ^ for start with of city and name
+
+    filter_backends = [OrderingFilter,SearchFilter] 
+    ordering_fields = ['name','city']
+    search_fields = ['^city','^name']
+   
 
     ''' def get_queryset(self):
         user = self.request.user
