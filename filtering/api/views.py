a3ca79768ad20d76a2640dd4a5d7e48c353782f5
anujneupane <anujneupane987@gmail.com>
2025-03-18 13:07:10 +0545

    use-search-filter

--- filtering/api/views.py
+++ filtering/api/views.py
@@ -3,13 +3,16 @@ from .serializer import RecordSerializer
 from rest_framework.generics import ListAPIView
 from .models import Record
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 # Create your views here.
 class RecoredApi(ListAPIView):
-    serializer_class = RecordSerializer
     queryset = Record.objects.all()
-    filter_backends = [DjangoFilterBackend] 
-    filterset_fields = ['passby','city','roll']
-
+    serializer_class = RecordSerializer
+    #filter_backends = [DjangoFilterBackend] 
+    #filterset_fields = ['passby','city','roll']
+    filter_backends = [SearchFilter] 
+    #search_fields = ['city','name']
+    search_fields = ['^city','^name']  # ^ for start with of city and name
 
     ''' def get_queryset(self):
         user = self.request.user
--- filtering/filtering/settings.py
+++ filtering/filtering/settings.py
@@ -116,4 +116,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 '''REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'SEARCH_PARAM': 'find'
 } '''
