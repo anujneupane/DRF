ea2ec3bb5e45c0e0c30e771358d45613158a2d13
anujneupane <anujneupane987@gmail.com>
2025-03-19 12:33:42 +0545

    setup-serializerRelation-models

---
+++ SerializerRelation/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Singer(models.Model):
+    name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    singer = models.ForeignKey(Singer,on_delete=models.CASCADE)  
+    duration = models.IntegerField()  
+
+    def __str__(self):
+       return self.title
+
