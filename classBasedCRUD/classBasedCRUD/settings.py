36d562937ebb71e3e921f06df3177cd29d7e21a8
anujneupane <anujneupane987@gmail.com>
2025-03-20 16:32:29 +0545

    back-to-normal

--- classBasedCRUD/classBasedCRUD/settings.py
+++ classBasedCRUD/classBasedCRUD/settings.py
@@ -125,6 +125,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
+'''' REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES':('rest_framework.renderers.JSONRenderer',)
-}
+} '''
